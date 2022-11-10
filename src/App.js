20ec3d065c3107d6bbb09595d0957fb0c1b1c36c
Y0ni0 <eshcolsam@gmail.com>
2022-11-10 14:14:48 +0300

    the task and tasks component added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
 
 import Header from "./Components/Header"
+import Tasks from "./Components/Tasks"
 import React from 'react';
+import { useState } from 'react'
+
 
 function App() {
+    const [tasks, setTasks] = useState([
+        {
+            id: 1,
+            text: 'Doctors Appointment',
+            day: 'Feb 5th at 2:30pm ',
+            reminder: true,
+        },
+        {
+            id: 2,
+            text: 'Meeting at school',
+            day: 'Feb 6th at 1:30pm ',
+            reminder: true,
+        },
+        {
+            id: 3,
+            text: 'Food Shoping',
+            day: 'Feb 5th at 2:30pm ',
+            reminder: false,
+        }
+    ])
+    //Delete task
+    const deleteTask = (id) => {
+        console.log('delete', id)
+    }
+
     return (
         <div className='container'>
             <Header />
+            <Tasks tasks={tasks} onDelete={deleteTask} />
         </div>
     )
 }
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,13 +1,16 @@
 import PropTypes from 'prop-types'
-
 import Button from './Button'
-const Header = ({title}) => {
+import React from 'react'
+
+const Header = ({ title }) => {
+  const onClick = () => {
+    console.log('click')
+  }
   return (
-   <header className='header'>
-       <h1>{title}
-       <Button />
-       </h1>
-   </header>
+    <header className='header'>
+      <h1>{title}</h1>
+      <Button color='green' text='Add' onClick={onClick} />
+    </header>
   )
 }
 
